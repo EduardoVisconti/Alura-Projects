d3ee2eaee6dad3347d015cf74a3733efdf356b2f
Eduardo Visconti <duduhsg@gmail.com>
2024-08-04 10:51:01 -0400

    Enhance sound playback function and add keyboard event listeners
    - Improved `tocaSom` function to include validation for audio elements
    - Added keyboard event listeners to play sound on 'Space' and 'Enter' key presses
    - Added visual feedback for key press events using 'ativa' class
    - Replaced while loop with a for loop for cleaner iteration through keys

--- aluramidi-2/main.js
+++ aluramidi-2/main.js
@@ -1,21 +1,31 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio)
+
+  if (elemento != null && elemento.localName === 'audio') {
+    elemento.play()
+  } else {
+    console.log('Elemento não encontrado ou seletor inválido')
+  }
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador]
+  const instrumento = tecla.classList[1]
+  const idAudio = `#som_${instrumento}`
 
-while (contador < listaDeTeclas.length) {
+  tecla.onclick = function () {
+    tocaSom(idAudio)
+  }
 
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-
-    const idAudio = `#som_${instrumento}`;
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa')
     }
+  }
 
-    contador = contador + 1;
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa')
+  }
 }
